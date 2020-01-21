289afa1e04ae1797bfd4614bfbfc557140d54596
Sifou Biri <sbiri@fluxym.com>
2020-01-21 09:07:52 -0500

    more changes

--- src/main/run.clj
+++ src/main/run.clj
@@ -1,59 +1,59 @@
-(ns run
-  (:require ;[com.wsscode.pathom.connect :as pc]
-                                        ; [taoensso.timbre :as log]
+;; (ns run
+;;   (:require ;[com.wsscode.pathom.connect :as pc]
+;;                                         ; [taoensso.timbre :as log]
    
    
    
    
    
    
-                                        ;[datomic.client.api :as d]
-   [datomic.api :as d]
-   [app.model.api :as api]
+;;                                         ;[datomic.client.api :as d]
+;;    [datomic.api :as d]
+;;    [app.model.api :as api]
    
    
    
-                                        ;[clojure.data.generators :as gen]
+;;                                         ;[clojure.data.generators :as gen]
    
-   [clojure.pprint :as pp]
-                                        ;[app.model.database :as db :refer [conn]]
-   [overtone.at-at :as at-at]
-   [clojure.core :refer :all]
-   [tick.alpha.api :as t])
-  )
+;;    [clojure.pprint :as pp]
+;;                                         ;[app.model.database :as db :refer [conn]]
+;;    [overtone.at-at :as at-at]
+;;    [clojure.core :refer :all]
+;;    [tick.alpha.api :as t])
+;;   )
 
 
 
-(defn run []
-  (println "delete projects...........")
-  (d/transact
-   (d/connect "datomic:dev://localhost:4334/one2")
+;; (defn run []
+;;   (println "delete projects...........")
+;;   (d/transact
+;;    (d/connect "datomic:dev://localhost:4334/one2")
 
-   (mapv
-    (fn [id]
+;;    (mapv
+;;     (fn [id]
 
-      [:db/retractEntity id])
+;;       [:db/retractEntity id])
 
-    (d/q '[:find [?e ...]
+;;     (d/q '[:find [?e ...]
 
-           :where
-           [?e :project/name ?n]
+;;            :where
+;;            [?e :project/name ?n]
 
-           ] (d/db (d/connect "datomic:dev://localhost:4334/one2")))))
+;;            ] (d/db (d/connect "datomic:dev://localhost:4334/one2")))))
 
-  (println "seed projects...............")
-  @(d/transact (d/connect "datomic:dev://localhost:4334/one2") (api/get-all-projects))
+;;   (println "seed projects...............")
+;;   @(d/transact (d/connect "datomic:dev://localhost:4334/one2") (api/get-all-projects))
 
-  (println "Done....................")
-  )
-
-
-
-(def my-pool (at-at/mk-pool))
-(at-at/interspaced (t/millis (t/new-duration 120 :minutes)) run my-pool)
-
-;(t/millis (t/between (t/instant "1970-01-01T00:00:00Z") (t/now)))
+;;   (println "Done....................")
+;;   )
 
 
 
-;(System/exit 0)
+;; (def my-pool (at-at/mk-pool))
+;; (at-at/interspaced (t/millis (t/new-duration 120 :minutes)) run my-pool)
+
+;; ;(t/millis (t/between (t/instant "1970-01-01T00:00:00Z") (t/now)))
+
+
+
+;; ;(System/exit 0)
