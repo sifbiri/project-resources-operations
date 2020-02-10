bad9d83a3364e115348dcf3ea32b0ae9b929f0e1
Sifou Biri <sbiri@fluxym.com>
2020-02-10 09:49:39 -0500

    added sync schedule

--- src/main/app/model/api.clj
+++ src/main/app/model/api.clj
@@ -539,9 +539,9 @@
 
 
 
-                                        ;(def my-pool (at-at/mk-pool))
+(def my-pool (at-at/mk-pool))
 
-                                        ;(def schedule (at-at/every (t/millis (t/new-duration 400 :minutes)) update-db my-pool :initial-delay (t/millis (t/new-duration 400 :minutes))))
+(def schedule (at-at/every (t/millis (t/new-duration 250 :minutes)) update-db my-pool :initial-delay (t/millis (t/new-duration 400 :minutes))))
                                         ;(at-at/stop schedule)
                                         ;
                                         ;(transact-all (d/connect "datomic:dev://localhost:4334/one2") "resources/edn/schema.edn")
