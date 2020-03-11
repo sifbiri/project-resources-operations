2c3fff0d87f49bd2fe8061357327c9519f5eb458
Sifou Biri <sifeddinebiri@gmail.com>
2020-03-11 16:02:20 -0400

    c

--- src/main/app/ui/imports.cljs
+++ src/main/app/ui/imports.cljs
@@ -229,7 +229,7 @@
                    (mapv #(ui-table-header-cell {:style {:position "sticky" :top 0}} %) ["Type" "Time" "Start Period" "End Period" "File"])))
                  (ui-table-body
                   {}
-                  (mapv ui-import (sort-by :import/time t/> imports)))
+                  (mapv ui-import (sort-by :import/time t/< imports)))
                  (ui-table-footer
                   {}
                   (ui-table-row
